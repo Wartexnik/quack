c9c5d4a9525a1690f150cb429bc43b13d3a9be06
Oleksii Nakhod <alexey.nakhod@gmail.com>
2022-11-04 20:24:39 +0200

    Add click button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-document.querySelector('h1').innerHTML = "Hi There, World!"
+let cookies = 0;
+
+function updateUI() {
+    document.getElementById('cookie-count').innerHTML = cookies
+}
+
+document.getElementById('get-cookie').addEventListener('click', ()=>{
+    cookies += 1;
+    updateUI();
+})
